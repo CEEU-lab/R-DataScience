52caf0fcaf11b646b722ad40b63e322abd5d3ab2
Elian Soutullo <eliansoutul@gmail.com>
2023-10-17 19:07:19 -0300

    actualizo practica 1

--- practicas-guiadas/funciones.R
+++ practicas-guiadas/funciones.R
@@ -1,14 +1,15 @@
-estadia_promedio <- function(data, region, fecha){
+estadia_promedio <- function(data, region, year, trim){
   
   estadia <- data %>% 
-    group_by(trim_date, region_destino) %>% 
+    group_by(anio, trimestre, region_destino) %>% 
     summarise(turistas = sum(pondera, na.rm = T),
               pernoctes = sum(pondera * px07, na.rm = T)) %>% 
     ungroup() %>% 
     mutate(estadia_promedio = pernoctes/turistas) %>% 
     filter(region_destino == region,
-           trim_date == fecha) %>% 
-    select(trim_date, region_destino, estadia_promedio)
+           anio == year,
+           trimestre == trim) %>% 
+    select(anio, trimestre, region_destino, estadia_promedio)
   
   estadia
 }
